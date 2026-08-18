663fe74d51f220446bb948f73e33b50031044406
baal86 <43853528+baal86@users.noreply.github.com>
2026-08-18 20:09:18 +0200

    Enhance MQTT discovery with error handling
    Refactored sensor discovery process to include error handling for MQTT publishing.

--- groundwater2mqtt/discovery.py
+++ groundwater2mqtt/discovery.py
@@ -10,31 +10,33 @@ async def task(procdata):
     while 1:
         logging.debug("Discovering")
         logging.info("[{}] publishing HA discovery".format(id))
-
-        sensor_dict = {
-            "device": {
-                "identifiers": ["groundwater_{}".format(id.replace("-","_"))],
-                "name": "Groundwater {}".format(id),
-                "manufacturer": "Web Scraper",
-            },
-            "origin": {
-                "name": "Groundwater2MQTT"
-            },
-            "qos": 1,
-            "name": "Level",
-            "unique_id": id + "_level",
-            "state_topic": prefix + id + "/level",
-            "unit_of_measurement": "m",
-            "device_class": "distance",
-            "state_class": "measurement"
-        }
-        async with aiomqtt.Client(
-                hostname = procdata["server"],
-                port = procdata["port"],
-                username = procdata["user"],
-                password = procdata["password"],
-                clean_session=True
-                ) as client:  
-            await client.publish("homeassistant/sensor/{}/config".format(id + "_level"),payload="{}".format(json.dumps(sensor_dict)),qos=1,retain=True)
-
+        try:
+            sensor_dict = {
+                "device": {
+                    "identifiers": ["groundwater_{}".format(id.replace("-","_"))],
+                    "name": "Groundwater {}".format(id),
+                    "manufacturer": "Web Scraper",
+                },
+                "origin": {
+                    "name": "Groundwater2MQTT"
+                },
+                "qos": 1,
+                "name": "Level",
+                "unique_id": id + "_level",
+                "state_topic": prefix + id + "/level",
+                "unit_of_measurement": "m",
+                "device_class": "distance",
+                "state_class": "measurement"
+            }
+            async with aiomqtt.Client(
+                    hostname = procdata["server"],
+                    port = procdata["port"],
+                    username = procdata["user"],
+                    password = procdata["password"],
+                    clean_session=True
+                    ) as client:  
+                await client.publish("homeassistant/sensor/{}/config".format(id + "_level"),payload="{}".format(json.dumps(sensor_dict)),qos=1,retain=True)
+        except aiomqtt.MqttError as e:
+            logging.warning("MQTT discovery failed ")
+        
         await asyncio.sleep(750)
